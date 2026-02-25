dca47961af26595aad268d12efc1abfc7fd14918
mathanvijai-web <mathanvijai@gmail.com>
2026-02-26 02:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-from flask import Flask,jsonify
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-NSE_URL = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
+@app.route("/")
+def home():
+    return "Mathan AI Market Engine Running"
 
-headers = {
-    "User-Agent":"Mozilla/5.0",
-    "Accept-Language":"en-US,en;q=0.9"
-}
-
-def get_market_data():
-    session = requests.Session()
-    session.get("https://www.nseindia.com",headers=headers)
-    response=session.get(NSE_URL,headers=headers)
-    data=response.json()
-
-    ce_oi=0
-    pe_oi=0
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
